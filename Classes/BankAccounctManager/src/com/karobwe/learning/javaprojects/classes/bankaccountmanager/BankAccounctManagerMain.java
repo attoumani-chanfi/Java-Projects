959f677b857e49af4e9f02f68d40882fedcd4960
Chanfi Attoumani <attoumani.chanfi@netcourrier.com>
2017-07-12 13:49:31 +0200

    Initalisation of the Bank Account Manager project
    Why:
    
      * Create new project that will use Classes features for managing an account. (Done whith Netbeans 2.1)
    
    Files affected by the changement:
    
    	new file : Classes/BankAccounctManager/build.xml
    	new file : Classes/BankAccounctManager/manifest.mf
    	new file : Classes/BankAccounctManager/nbproject/build-impl.xml
    	new file : Classes/BankAccounctManager/nbproject/genfiles.properties
    	new file : Classes/BankAccounctManager/nbproject/project.properties
    	new file : Classes/BankAccounctManager/nbproject/project.xml
    	new file : Classes/BankAccounctManager/src/com/karobwe/learning/javaprojects/classes/bankaccountmanager/BankAccounctManagerMain.java

---
+++ Classes/BankAccounctManager/src/com/karobwe/learning/javaprojects/classes/bankaccountmanager/BankAccounctManagerMain.java
@@ -0,0 +1,16 @@
+package com.karobwe.learning.javaprojects.classes.bankaccountmanager;
+
+/**
+ *
+ * @author Chanfi Attoumani <attoumani.chanfi@netcourrier.com>
+ */
+public class BankAccounctManagerMain {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
